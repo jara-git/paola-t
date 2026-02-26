f3a2ed92d0a1868cfbd9629e3d9ebb8671c76f07
jara <jarabelmonte@gmail.com>
2026-02-26 14:41:55 +0100

    correciones puntuaciones y animacion  h1

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,24 +1,43 @@
-// src/components/AnimatedTitle.jsx
 import React from "react";
 import "../styles/_animations.scss";
 import useFadeInOnScroll from "../hooks/useFadeInOnScroll";
 
 export default function AnimatedTitle({ text, as = "h1", className = "" }) {
     const [ref, visible] = useFadeInOnScroll({ threshold: 0.1 });
-    const letters = Array.from(text);
 
-    const Component = as; // define la variable para JSX
+    const words = text.split(" ");
+    const Component = as;
+
+    let letterIndex = 0;
 
     return (
-        <Component ref={ref} className={`animated-title ${className} ${visible ? "is-visible" : ""}`}>
-            {letters.map((letter, i) => (
-                <span
-                    key={i}
-                    className="letter"
-                    style={{ transitionDelay: visible ? `${i * 60}ms` : "0ms" }}
-                >
-                    {letter === " " ? "\u00A0" : letter}
-                </span>
+        <Component
+            ref={ref}
+            className={`animated-title ${className} ${visible ? "is-visible" : ""}`}
+        >
+            {words.map((word, wordIndex) => (
+                <React.Fragment key={wordIndex}>
+                    <span className="word">
+                        {Array.from(word).map((letter, i) => {
+                            const delay = visible ? `${letterIndex * 60}ms` : "0ms";
+                            letterIndex++;
+
+                            return (
+                                <span
+                                    key={i}
+                                    className="letter"
+                                    style={{ transitionDelay: delay }}
+                                >
+                                    {letter}
+                                </span>
+                            );
+                        })}
+                    </span>
+
+                    {wordIndex < words.length - 1 && (
+                        <span className="space">&nbsp;</span>
+                    )}
+                </React.Fragment>
             ))}
         </Component>
     );
